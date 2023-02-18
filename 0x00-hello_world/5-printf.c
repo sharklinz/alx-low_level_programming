a973cde434156635e2b3bbec406392e1a3de9d49
bright <brightneedom@gmail.com>
2023-02-18 09:56:00 -0800

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Program used to print a line with the printf fucntion
- * Return: 0
+ * Return: 0 (success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	return (0);
 }
 
