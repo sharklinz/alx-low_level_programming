9d31b5ae1b71a0e2ee0d8c0695648df788280820
bright <brightneedom@gmail.com>
2023-02-18 07:49:06 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Its used to print a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
